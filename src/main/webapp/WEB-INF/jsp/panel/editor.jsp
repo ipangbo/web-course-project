48f186181b43c2130f3214f8c4f03faeabeca7d8
ipangbo <122402331@qq.com>
2021-12-12 13:25:48 +0800

    编辑器前端综合提升

--- src/main/webapp/WEB-INF/jsp/panel/editor.jsp
+++ src/main/webapp/WEB-INF/jsp/panel/editor.jsp
@@ -15,16 +15,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>编辑文章</title>
-    <link rel="shortcut icon" href="../../../favicon.ico" type="image/x-icon">
+    <c:url var="base" value="/"></c:url>
+    <base href="${base}">
+    <title>${action}</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css"
           integrity="sha384-cLRrMq39HOZdvE0j6yBojO4+1PrHfB7a9l5qLcmRm/fiWXYY+CndJPmyu5FV/9Tw" crossorigin="anonymous"/>
     <link href="https://cdn.jsdelivr.net/npm/@wangeditor/editor@latest/dist/css/style.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"
-            integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="../css/editor.css">
+    <link rel="stylesheet" href="css/editor.css">
 
 </head>
 
@@ -35,10 +33,12 @@
     " id="mdui-title-bar">
 <div class="mdui-appbar mdui-appbar-fixed">
     <div class="mdui-toolbar mdui-color-theme">
-        <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">menu</i></a>
+        <a href="javascript:;" class="mdui-btn mdui-btn-icon" id="drawer-toggle-button"><i class="mdui-icon material-icons">menu</i></a>
         <a href="javascript:;" class="mdui-typo-headline">磅礴新闻网管理系统</a>
         <a href="javascript:;" class="mdui-typo-title">${action}</a>
         <div class="mdui-toolbar-spacer"></div>
+        <a href="panel?page=search" class="mdui-btn mdui-btn-icon mdui-ripple" id="panel-search-button" target="_blank"><i
+                class="mdui-icon material-icons">search</i></a>
         <a href="javascript:;" class="mdui-btn mdui-btn-icon mdui-ripple" id="editor-save-button"><i
                 class="mdui-icon material-icons">save</i></a>
     </div>
@@ -102,8 +102,11 @@
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"
+        integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A"
+        crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@wangeditor/editor@latest/dist/index.min.js"></script>
-<script src="../js/editor.js"></script>
+<script src="js/editor.js"></script>
 <script src="js/panel-general.js"></script>
 
 </body>
--- src/main/webapp/js/editor.js
+++ src/main/webapp/js/editor.js
@@ -1,85 +1,85 @@
-$(function () {
-    const wangEditor = window.wangEditor; // 全局变量
-    const editorConfig = {MENU_CONF: {}};
-    editorConfig.placeholder = '请输入内容';
-    editorConfig.onChange = (editor) => {
-        // 当编辑器选区、内容变化时，即触发
-        console.log('content', editor.children)
-        console.log('html', editor.getHtml())
-    };
-    editorConfig.scroll = false // 禁止编辑器滚动
+var $ = mdui.$;
 
-    // 创建编辑器
-    const editor = wangEditor.createEditor({
-        selector: '#editor-text-area',
-        content: window.content1,
-        config: editorConfig,
-        content: [], // 默认内容，下文有解释
-        mode: 'default' // 或者 'simple' ，下文有解释
-    });
+const wangEditor = window.wangEditor; // 全局变量
+const editorConfig = {MENU_CONF: {}};
+editorConfig.placeholder = '请输入内容';
+editorConfig.onChange = (editor) => {
+    // 当编辑器选区、内容变化时，即触发
+    console.log('content', editor.children)
+    console.log('html', editor.getHtml())
+    const content = editor.children
+    const contentStr = JSON.stringify(content)
+    document.getElementById('textarea-1').innerHTML = contentStr
+    const html = editor.getHtml()
+    document.getElementById('textarea-2').innerHTML = html
+    console.log("文本框：" + document.getElementById('textarea-2').innerText)
+};
+editorConfig.scroll = false // 禁止编辑器滚动
 
-    // 创建工具栏
-    const toolbar = wangEditor.createToolbar({
-        editor,
-        selector: '#toolbar-container',
-        mode: 'default', // 或者 'simple' ，下文有解释
-        config: {
-            excludeKeys: 'fullScreen'
-        }
-    });
+// 创建编辑器
+const editor = wangEditor.createEditor({
+    selector: '#editor-text-area',
+    config: editorConfig,
+    content: [], // 默认内容，下文有解释
+    mode: 'default' // 或者 'simple' ，下文有解释
+});
 
-    // 点击空白处 focus 编辑器
-    document.getElementById('editor-text-area').addEventListener('click', e => {
-        if (e.target.id === 'editor-text-area') {
-            editor.blur()
-            editor.focus(true) // focus 到末尾
-        }
-    });
-
-    editorConfig.onChange = (editor) => {
-        const content = editor.children
-        const contentStr = JSON.stringify(content)
-        document.getElementById('textarea-1').innerHTML = contentStr
-
-        const html = editor.getHtml()
-        document.getElementById('textarea-2').innerHTML = html
+// 创建工具栏
+const toolbar = wangEditor.createToolbar({
+    editor,
+    selector: '#toolbar-container',
+    mode: 'default', // 或者 'simple' ，下文有解释
+    config: {
+        excludeKeys: ['fullScreen', 'uploadImage']
     }
+});
 
-    const editorConfig2 = {MENU_CONF: {}};
-    editorConfig2.placeholder = '请输入摘要';
-    editorConfig2.onChange = (editor) => {
-        // 当编辑器选区、内容变化时，即触发
-        console.log('content', editor.children)
-        console.log('html', editor.getHtml())
-    };
-    editorConfig2.scroll = false // 禁止编辑器滚动
-    // 创建编辑器
-    const abstractEditor = wangEditor.createEditor({
-        selector: '#editor-abstract-area',
-        content: window.content1,
-        config: editorConfig2,
-        content: [], // 默认内容，下文有解释
-        mode: 'default' // 或者 'simple' ，下文有解释
-    });
+// 点击空白处 focus 编辑器
+document.getElementById('editor-text-area').addEventListener('click', e => {
+    if (e.target.id === 'editor-text-area') {
+        editor.blur()
+        editor.focus(true) // focus 到末尾
+    }
+});
 
-    var instHelp = new mdui.Dialog('#editor-help-dialog');
-    $("#editor-help-button").click(function () {
-        instHelp.open()
-    })
-    $("#editor-help-dialog-ok").click(function () {
-        instHelp.close()
-    })
-    var instSave = new mdui.Dialog('#editor-save-dialog');
-    $("#editor-save-button").click(function () {
-        $("#save-spinner").hide()
-        instSave.open()
-    })
-    $("#editor-save-dialog-ok").click(function () {
+
+const editorConfig2 = {MENU_CONF: {}};
+editorConfig2.placeholder = '请输入摘要。请注意，新闻网只能显示约150字，多余会被折叠。';
+editorConfig2.onChange = (editor) => {
+    console.log('content', editor.children)
+    console.log('html', editor.getHtml())
+};
+editorConfig2.scroll = false // 禁止编辑器滚动
+// 创建编辑器
+const abstractEditor = wangEditor.createEditor({
+    selector: '#editor-abstract-area',
+    config: editorConfig2,
+    content: [], // 默认内容，下文有解释
+    mode: 'simple' // 或者 'simple' ，下文有解释
+});
+
+var instHelp = new mdui.Dialog('#editor-help-dialog');
+$("#editor-help-button").on('click', function () {
+    instHelp.open()
+})
+$("#editor-help-dialog-ok").on('click', function () {
+    instHelp.close()
+})
+var instSave = new mdui.Dialog('#editor-save-dialog');
+$("#editor-save-button").on('click', function () {
+    $("#save-spinner").hide()
+    instSave.open()
+})
+$("#editor-save-dialog-ok").on('click', function () {
+    if (editor.isEmpty()) {
+        instSave.close()
+        mdui.alert("当前内容为空，必须写入内容才能提交")
+    } else {
         $("#editor-save-dialog-ok").css("visibility", "hidden")
         $("#editor-save-dialog-cancel").hide()
         $("#save-spinner").show()
-    })
-    $("#editor-save-dialog-cancel").click(function () {
-        instSave.close()
-    })
+    }
+})
+$("#editor-save-dialog-cancel").on('click', function () {
+    instSave.close()
 })
