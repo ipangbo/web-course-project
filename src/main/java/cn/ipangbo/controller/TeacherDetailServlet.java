1d895a9e35cc90711dda1aab8bb8804e12c129e5
ipangbo <122402331@qq.com>
2021-12-13 23:04:51 +0800

    教师界面空指针修复

---
+++ src/main/java/cn/ipangbo/controller/TeacherDetailServlet.java
@@ -0,0 +1,17 @@
+package cn.ipangbo.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/contents/teachers/detail")
+public class TeacherDetailServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int tid = Integer.parseInt(req.getParameter("tid"));
+        req.getRequestDispatcher("/WEB-INF/jsp/teacher.jsp").forward(req, resp);
+    }
+}
