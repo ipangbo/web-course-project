2feffb2d9a39ad79dbff8525a6ef35f2e77fce7e
ipangbo <122402331@qq.com>
2021-12-17 23:45:49 +0800

    加入notice的服务

--- src/main/webapp/js/panel-search.js
+++ src/main/webapp/js/panel-search.js
@@ -28,7 +28,7 @@ $("#search-button").on('click', () => {
         .then(res => {
             // 返回的内容是[{}, {}]
             res.forEach(item => {
-
+                // todo: 有cate信息吗？
                 let listItem = `
         <div class="mdui-list-item" data-aid="${item.id}">
             <label class="mdui-checkbox">
